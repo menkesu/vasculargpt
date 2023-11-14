41f7cb0d48b398285215cb424c0f73aefa7a39f4
menkesu <55628366+menkesu@users.noreply.github.com>
2023-11-14 15:56:56 +0000

    remove dotenv

--- Hello.py
+++ Hello.py
@@ -5,8 +5,7 @@ from langchain.chains import ConversationalRetrievalChain
 from langchain.vectorstores import Pinecone
 from langchain.embeddings import OpenAIEmbeddings
 from langchain.chat_models import ChatOpenAI
-from dotenv import load_dotenv
-load_dotenv()
+
 # Set up page configuration
 st.set_page_config(page_title="VascularGPT", layout="wide")
 
